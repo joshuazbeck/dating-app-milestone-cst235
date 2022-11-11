3abc50c77d8c8eff3e1f13a85a1084d64ae7fd91
tray15 <tray15@my.gcu.edu>
2022-11-11 16:08:07 -0700

    update
    update
    added mysqlconnector.jar

--- src/main/java/business/DatabaseService2.java
+++ src/main/java/business/DatabaseService2.java
@@ -16,8 +16,7 @@ import beans.DatingUser;
 import beans.User;
 
 /**
- * The service used to emulate the database as of Milestone 2
- * @author Josh Beck
+ * @author Tanner Ray
  *
  */
 @Stateless
@@ -30,16 +29,38 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 	private static final String DB_USER = "root";
 	private static final String PASSWORD = "root";
 	
-	private static final String INSERT_USER = "INSERT INTO users () VALUES (?)";
-	private static final String FIND_BY_ID = "SELECT * FROM users WHERE id=?";
-	private static final String FIND_ALL = "SELCT * FROM users ORDER BY id";
-	private static final String UPDATE = "UPDATE users SET first_name=?, last_name=?, email_address=?, phone_number=?, password=?, dating_user=?";
-	private static final String DELETE = "DELETE from users WHERE id=?";
+	//User table queries
+	private static final String INSERT_USER = "INSERT INTO User () VALUES ()";
+	private static final String FIND_BY_ID = "SELECT * FROM User WHERE id=?";
+	private static final String FIND_ALL = "SELCT * FROM User ORDER BY id";
+	private static final String UPDATE = "UPDATE User SET ";
+	private static final String DELETE = "DELETE FROM User WHERE id=?";
+	
+	//DatingUser table queries
+	private static final String INSERT_DU = "INSERT INTO DatingUser () VALUES ()";
+	private static final String FIND_DU_ID = "";
+	private static final String FIND_ALL_DU = "";
+	private static final String UPDATE_DU = "UPDATE DatingUser SET ";
+	private static final String DELETE_DU = "DELETE FROM DatingUser WHERE user_id";
+	
+	//Hobby table queries
+	private static final String INSERT_HOBBY = "";
+	private static final String FIND_HOBBY_ID = "";
+	private static final String FIND_ALL_HOBBIES = "";
+	private static final String UPDATE_HOBBY = "";
+	private static final String DELETE_HOBBY = "";
+	
+	//Address table queries
+	private static final String INSERT_ADDRESS = "";
+	private static final String FIND_ADDRESS_ID = "";
+	private static final String UPDATE_ADDRESS = "";
+	private static final String DELETE_ADDRESS = "";
 	
 	/**
 	 * Add a user to the database
 	 * @param user - User
 	 */
+	@Override
 	public void addUsers(User user) {
 		Connection conn = null;
 		PreparedStatement stmt = null;
@@ -52,8 +73,8 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 			stmt.setObject(4, user.getPhoneNumber());
 			stmt.setString(5, user.getUsername());
 			stmt.setString(6, user.getPassword());
-			stmt.setObject(7, user.getDatingUser());
 			
+			stmt.executeQuery();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
@@ -61,6 +82,10 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 			close(conn);
 		}
 	}
+	/**
+	 * retrieve all users from the database
+	 * @param user - User
+	 */
 	@Override
 	public User findAllUsers(User user) {
 		Connection conn = null;
@@ -69,7 +94,7 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 			conn = getConnection();
 			stmt = conn.prepareStatement(FIND_ALL, Statement.RETURN_GENERATED_KEYS);
 			
-			ResultSet rs = stmt.getGeneratedKeys();
+			ResultSet rs = stmt.executeQuery();
 			
 			if (rs.next()) {
 				user.getId();
@@ -83,6 +108,10 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 			close(conn);
 		}
 	}
+	/**
+	 * retrieve one user from database from given ID
+	 * @param user - User
+	 */
 	@Override
 	public User findUserById(User user) {
 		Connection conn = null;
@@ -109,7 +138,10 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 			close(conn);
 		}
 	}
-	
+	/**
+	 * update a user in the database
+	 * @param user - User
+	 */
 	@Override
 	public void updateUser(User user) {
 		Connection conn = null;
@@ -124,6 +156,26 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 			stmt.setString(5, user.getPassword());
 			stmt.setObject(6, user.getDatingUser());
 			
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			close(stmt);
+			close(conn);
+		}
+	}
+	/**
+	 * delete a user from the database
+	 * @param user - User
+	 */
+	@Override
+	public void deleteUser(User user) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try {
+			conn = getConnection();
+			stmt = conn.prepareStatement(DELETE, Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, user.getId());
 			
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -134,18 +186,76 @@ public class DatabaseService2 implements DatabaseServiceInterface2 {
 	}
 	
 	@Override
-	public void deleteUser(User user) {
+	public void addDatingUser(User user) {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try {
 			conn = getConnection();
-			stmt = conn.prepareStatement(DELETE, Statement.RETURN_GENERATED_KEYS);
+			stmt = conn.prepareStatement(INSERT_DU, Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(0, 0);
+					
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			close(stmt);
-			close(conn);
 		}
+		
+	}
+	@Override
+	public DatingUser findAllDatingUsers(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public DatingUser findDatingUserById(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void updateDatingUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deleteDatingUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void addHobby(DatingUser du) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void updateHobby(DatingUser du) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deleteHobby(DatingUser du) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public void addAddress(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void updateAddress(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deleteAddress(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void findAddress(User user) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	private Connection getConnection() {
--- src/main/java/business/DatabaseServiceInterface2.java
+++ src/main/java/business/DatabaseServiceInterface2.java
@@ -1,15 +1,28 @@
 package business;
 
+import beans.DatingUser;
 import beans.User;
 
 public interface DatabaseServiceInterface2 {
-
+	
+	void addUsers(User user);
 	User findAllUsers(User user);
-
 	User findUserById(User user);
-
 	void updateUser(User user);
-
 	void deleteUser(User user);
-
+	
+	void addDatingUser(User user);
+	DatingUser findAllDatingUsers (User user);
+	DatingUser findDatingUserById (User user);
+	void updateDatingUser (User user);
+	void deleteDatingUser (User user);
+	
+	void addHobby(DatingUser du);
+	void updateHobby(DatingUser du);
+	void deleteHobby(DatingUser du);
+	
+	void addAddress(User user);
+	void findAddress(User user);
+	void updateAddress(User user);
+	void deleteAddress(User user);
 }
